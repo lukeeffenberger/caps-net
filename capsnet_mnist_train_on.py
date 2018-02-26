102dfe52d67f5664a7c083d0f23eee8a9baded69
lukeeffenberger <leffenberger@uos.de>
2018-02-26 16:25:41 +0100

    do not intialite variables when restore variables

--- capsnet_mnist_train_on.py
+++ capsnet_mnist_train_on.py
@@ -122,8 +122,6 @@ def main():
         # validation loss to the best validation loss so far and store the
         # weights only if the mode was better.
         best_validation_loss = 0.015
-        # Initialize all variables.
-        sess.run(tf.global_variables_initializer())
 
         # Train the network for the specified number of epochs.
         for epoch in range(EPOCHS):
